8e93ae4813722bdb4c5db65a36ca9c89a87a2579
Kristina Ilyovska <26176057+lyloster@users.noreply.github.com>
2023-02-10 18:24:44 -0500

    Make a trailAPI request

---
+++ react-hiking/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HikeList from './components/HikeList.js';
+
+function App() {
+  return (
+    <div className="App">
+      <HikeList/>
+    </div>
+  );
+}
+
+export default App;
