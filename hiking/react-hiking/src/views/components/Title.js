7639ca55008bdbd66c7745dd9755d68bd16630a1
Kristina Ilyovska <26176057+lyloster@users.noreply.github.com>
2023-05-09 17:24:47 -0400

    Fix Search button

--- hiking/react-hiking/src/views/components/Title.js
+++ hiking/react-hiking/src/views/components/Title.js
@@ -1,79 +1,96 @@
-// src/Title.js
-import React from 'react'
-import { useState} from "react";
-import logo from '../../img/logo_mountain.png';
-
-import { convertLocationToCoord} from '../../services/convertLocationToCoord';
-import { handleHikeList } from '../../services/handleHikeList';
-
 import './Title.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+//img
+import logo from '../../img/logo_mountain.png';
 
-function Title({setResults}) {
+import React from 'react'
+import { useState} from "react";
 
-  const [inputValue, setInputValue] = useState();
+//routing
+import { useNavigate } from 'react-router-dom';
 
-  function fetchData(value) {
-    console.log("location (before convert function)= " + value);
-    convertLocationToCoord(value).then((coordinates) => {
-      console.log("coordinates " + coordinates);
-      let hikeList = handleHikeList(coordinates[0], coordinates[1]); 
-      return hikeList;
-    }).then((hikeList) => {
-      setResults(hikeList);
-    });
+//services
+import { convertLocationToCoord} from '../../services/convertLocationToCoord';
+import { handleHikeList } from '../../services/handleHikeList';
+
+function Title({ setResults }) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
   };
 
-  const handleChange = (value) => {
-    console.log(value);
-    setInputValue(value);
-    fetchData(value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchData(inputValue);
+  };
+  
+  const redirectToHomePage = () => {
+    window.location.href = '/'; // Redirect to the home page
+  };
+
+  const fetchData = (value) => {
+    convertLocationToCoord(value)
+      .then((coordinates) => {
+        return handleHikeList(coordinates[0], coordinates[1]);
+      })
+      .then((hikeList) => {
+        setResults(hikeList);
+        redirectToHomePage();
+      });
   };
 
   const isAuthenticated = localStorage.getItem('session_cookie_name');
- 
+
   return (
-    <div className="Title" >
+    <div className="Title">
       <div className="btn-group" role="group">
         <nav className="navbar navbar-light bg-light">
           <div className="container-fluid">
-            <form className="d-flex">
-            <a href="/"><img className="Logo" src={logo}/></a>
-            <input className="form-control me-2" 
-                  type="search" 
-                  placeholder="Search" 
-                  aria-label="Search"   
-                  value={inputValue} 
-                  onChange={(event) => handleChange(event.target.value)}/>
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <form className="d-flex" onSubmit={handleSubmit}>
+              <a href="/">
+                <img className="Logo" src={logo} />
+              </a>
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                value={inputValue}
+                onChange={handleChange}
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
             </form>
           </div>
-        </nav></div>
-        <div class="btn-group">
-          <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        </nav>
+      </div>
+      <div class="btn-group">
+        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Menu
+        </button>
+        <div class="dropdown-menu dropdown-menu-right">
+          <button class="dropdown-item" type="button">
+            <a class="navbar-brand" href="/">Home</a>
           </button>
-          <div class="dropdown-menu dropdown-menu-right">
-            <button class="dropdown-item" type="button">
-              <a class="navbar-brand" href="/">Home</a>
-            </button>
-            <button class="dropdown-item" type="button">
-              <a class="navbar-brand" href="/profile">Profile</a>
-            </button>
-            <button class="dropdown-item" type="button" >
-              <a class="navbar-brand" href="/settings">Settings</a>
-            </button>
-            {isAuthenticated ? (
-              <button class="dropdown-item" type="button"> <a class="navbar-brand" href="/bye">Logout</a></button>) 
-              : 
-              (<button class="dropdown-item" type="button"><a class="navbar-brand" href="/hi">Login</a></button>)
-            }
-          </div>
+          <button class="dropdown-item" type="button">
+            <a class="navbar-brand" href="/profile">Profile</a>
+          </button>
+          <button class="dropdown-item" type="button">
+            <a class="navbar-brand" href="/settings">Settings</a>
+          </button>
+          {isAuthenticated ? (
+            <button class="dropdown-item" type="button"> <a class="navbar-brand" href="/bye">Logout</a></button>
+          ) : (
+            <button class="dropdown-item" type="button"><a class="navbar-brand" href="/hi">Login</a></button>
+          )}
         </div>
+      </div>
     </div>
-  )
+  );
 }
-export default Title;
 
+export default Title;
